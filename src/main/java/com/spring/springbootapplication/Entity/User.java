71095776337de6e189c97686308920c0b3a56fbc
kotamuranaka-cyber <kurageyorikikurage@gmail.com>
2026-07-23 22:19:17 +0900

    fix:学習時間編集の空欄保存を不可に変更

--- src/main/java/com/spring/springbootapplication/Entity/User.java
+++ src/main/java/com/spring/springbootapplication/Entity/User.java
@@ -9,7 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 /* import jakarta.persistence.Lob; */
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
+/* import jakarta.validation.constraints.Email; */
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
